68d1d26d439a3c149b01414ab7cb9eb40aac52a0
davidmcdougal <davidbyu6@gmail.com>
2024-11-01 12:52:54 -0600

    finished the app

--- src/app/polls/[id].tsx
+++ src/app/polls/[id].tsx
@@ -10,35 +10,77 @@ import {
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { useEffect, useState } from "react";
-import { Poll } from "../../types/db";
+import { Poll, Vote } from "../../types/db";
 import { supabase } from "../../lib/supabase";
+import { useAuth } from "../providers/AuthProvider";
 
 export default function PollDetails() {
   const { id } = useLocalSearchParams<{ id: string }>();
   const [poll, setPoll] = useState<Poll>(null);
+  const [userVote, setUserVote] = useState<Vote>(null);
+
   const [selected, setSelected] = useState("");
 
-  const vote = () => {
-    console.log("Vote", selected);
-  };
+  const { user } = useAuth();
 
   useEffect(() => {
     const fetchPolls = async () => {
-      console.log("Fetching ...");
-
       let { data, error } = await supabase
         .from("polls")
         .select("*")
         .eq("id", Number.parseInt(id))
         .single();
       if (error) {
-        Alert.alert("Error", error.message);
+        Alert.alert("Error fetching data");
       }
       setPoll(data);
     };
 
+    const fetchUserVote = async () => {
+      if (!user) {
+        return;
+      }
+      let { data, error } = await supabase
+        .from("votes")
+        .select("*")
+        .eq("poll_id", Number.parseInt(id))
+        .eq("user_id", user.id)
+        .limit(1)
+        .single();
+
+      setUserVote(data);
+      if (data) {
+        setSelected(data.option);
+      }
+    };
+
     fetchPolls();
+    fetchUserVote();
   }, []);
+
+  const vote = async () => {
+    const newVote = {
+      option: selected,
+      poll_id: poll.id,
+      user_id: user?.id,
+    };
+    if (userVote) {
+      newVote.id = userVote.id;
+    }
+    const { data, error } = await supabase
+      .from("votes")
+      .upsert([newVote])
+      .select()
+      .single();
+
+    if (error) {
+      console.log(error);
+      Alert.alert("Failed to vote");
+    } else {
+      setUserVote(data);
+      Alert.alert("Thank you for your vote");
+    }
+  };
 
   if (!poll) {
     return <ActivityIndicator />;
@@ -46,8 +88,10 @@ export default function PollDetails() {
 
   return (
     <View style={styles.container}>
-      <Stack.Screen options={{ title: poll.question }} />
+      <Stack.Screen options={{ title: "Poll voting" }} />
+
       <Text style={styles.question}>{poll.question}</Text>
+
       <View style={{ gap: 5 }}>
         {poll.options.map((option) => (
           <Pressable
@@ -57,21 +101,28 @@ export default function PollDetails() {
           >
             <Feather
               name={option === selected ? "check-circle" : "circle"}
-              size={20}
+              size={18}
               color={option === selected ? "green" : "gray"}
             />
             <Text>{option}</Text>
           </Pressable>
         ))}
       </View>
+
       <Button onPress={vote} title="Vote" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { padding: 10, gap: 20 },
-  question: { fontSize: 20, fontWeight: "600" },
+  container: {
+    padding: 10,
+    gap: 20,
+  },
+  question: {
+    fontSize: 20,
+    fontWeight: "600",
+  },
   optionContainer: {
     backgroundColor: "white",
     padding: 10,
